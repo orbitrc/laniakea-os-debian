d3d43caf22cacb34906650f1ef2c2381616fc8d2
Gene Ryu <hardboiled65@gmail.com>
2019-04-15 12:27:31 +0900

    Add packages.

---
+++ download.sh
@@ -0,0 +1,7 @@
+LIBC_VERSION="2.27"
+UTIL_LINUX_VERSION="2.31.1"
+
+wget https://bintray.com/orbit/laniakea/download_file?file_path=libc6%2Flibc6_${LIBC_VERSION}_amd64.tar.xz -O src/packages/libc6_${LIBC_VERSION}_amd64.tar.xz
+
+wget https://bintray.com/orbit/laniakea/download_file?file_path=util-linux%2Futil-linux_${UTIL_LINUX_VERSION}_amd64.tar.xz -O src/packages/util-linux_${UTIL_LINUX_VERSION}_amd64.tar.xz
+
