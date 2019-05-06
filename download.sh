947937e8454c3b98f23ec2a942c89ef7a4123391
Gene Ryu <hardboiled65@gmail.com>
2019-05-06 11:58:50 +0900

    Add make package.

--- download.sh
+++ download.sh
@@ -4,3 +4,5 @@ wget https://bintray.com/orbit/laniakea/download_file?file_path=libc6%2Flibc6_${
 
 wget https://bintray.com/orbit/laniakea/download_file?file_path=util-linux%2Futil-linux_${UTIL_LINUX_VERSION}_amd64.tar.xz -O packages/util-linux_${UTIL_LINUX_VERSION}_amd64.tar.xz
 
+wget https://bintray.com/orbit/laniakea/download_file?file_path=make%2Fmake_${MAKE_VERSION}_amd64.tar.xz -O packages/make_${MAKE_VERSION}_amd64.tar.xz
+
